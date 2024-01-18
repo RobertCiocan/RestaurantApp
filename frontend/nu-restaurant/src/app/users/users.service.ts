e03886ca4e0ef609a7ad518a3c229718d7ad7a5d
Irina Rotaru <irina.rotaru@student.tuiasi.ro>
2024-01-18 11:01:25 +0200

    front si back register

--- frontend/nu-restaurant/src/app/users/users.service.ts
+++ frontend/nu-restaurant/src/app/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from './models/user.model';
+import { ClientRequest } from './models/Requests/client_request';
 
 @Injectable({
   providedIn: 'root',
@@ -9,11 +10,15 @@ import { User } from './models/user.model';
 export class UsersService {
   // ex de api pt a testa 
   private apiUrl = 'https://jsonplaceholder.typicode.com/users';
-
+  private apiUrlR ='http://localhost:8084/api';
   constructor(private http: HttpClient) {}
 
   registerUser(user: User): Observable<User> {
     return this.http.post<User>(this.apiUrl, user);
+  }
+
+  createClient(registration: ClientRequest): Observable<ClientRequest> {
+    return this.http.post<ClientRequest>(`${this.apiUrlR}/create_client`, registration);
   }
 
   signInUser(user: User): Observable<User> {
